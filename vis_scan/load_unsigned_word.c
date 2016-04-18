b8fe8ed4470738f78a20c24ed43eff3285f3c5dc
Jonathan Sid-Otmane <jonathan_sid-otmane@brown.edu>
2016-04-18 16:35:14 -0400

    [MOD] VIS Makefile and (un)load word

---
+++ vis_scan/load_unsigned_word.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <vis.h>
+/** Test of the VIS api on oracle M7
+ *
+ * Load an unsigned value to a register and get it back.
+ *
+ * Since there is no way of storing a big endian  value, 
+ * the storing process ;ust be done in little endian.
+ *
+ * Also, all loading and storing must be done exclusively on 
+ * aligned memory>
+ *
+ * Also, all loading and storing must be done exclusively on 8 bits
+ * aligned memory.
+ *
+ */
+
+int main(){
+
+	unsigned int *n, *m;
+	int i;
+
+	if(posix_memalign( (void **) &n, 8, sizeof(int)))
+		return -1;
+	if(posix_memalign( (void **) &m, 8, sizeof(int)))
+		return -1;
+
+	*n = 45;
+	*m = 0;
+
+	// memory -> register
+	 i = vis_ld_u64_nf_le(n);
+
+	 printf("Put value %d, got %d\n", *n, i);
+
+	 // register -> memory
+	 __vis_st_uint64_le(i, m);
+	 printf("Done! the new value is %d\n", *m);
+
+
+	return 0;
+}
+
+
