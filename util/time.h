a8382692b4cc0b0365e828540164a1bc96fd4cdb
Jonathan Sid-Otmane <jonathan_sid-otmane@brown.edu>
2016-06-14 13:33:10 -0400

    Bitweaving range scan

--- util/time.h
+++ util/time.h
@@ -8,17 +8,19 @@ static __inline__ unsigned long long tick(void){
 	unsigned hi, lo;
 	__asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
 	return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
-#elif
+#else
 	return 0;
 #endif
 }
 
 static inline unsigned long long timestamp() {
+#ifdef __gnu_linux__
         struct timespec ts;
-//#ifdef LINUX 
         clock_gettime(CLOCK_MONOTONIC, &ts);
-//#endif
         return ts.tv_sec * 1e9 + ts.tv_nsec;
+#else
+	return gethrtime();
+#endif
 }
 
 
