a07a1a6b1f47101e046a57f70d2da53fb617e1bc
Jonathan Sid-Otmane <Watabanga@gmail.com>
2016-06-14 13:11:21 -0400

    Repairs broken time

--- util/time.h
+++ util/time.h
@@ -4,9 +4,13 @@
 #include <time.h>
 
 static __inline__ unsigned long long tick(void){
+#ifdef __gnu_linux__
 	unsigned hi, lo;
 	__asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
 	return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
+#elif
+	return 0;
+#endif
 }
 
 static inline unsigned long long timestamp() {
